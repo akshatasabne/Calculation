776038551c251c1b5afd26ed679d415e72cf156c
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-06-20 18:14:05 +0530

    Add project files.

---
+++ Calculation/Airthmatical.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    public class Airthmatical
+    {
+        public int a, b, c;
+
+        public int Addition(int a, int b)
+        {
+            return a + b;
+            
+        }
+        public int Addition(int a,int b,int c)
+        {
+            return a + b + c;
+            
+        }
+        public double Addition(double a, double b)
+        {
+            return a + b;
+            
+        }
+        public int Subtraction(int a, int b)
+        {
+            return a -b;
+
+        }
+        public int Subtraction(int a, int b, int c)
+        {
+            return a - b - c;
+
+        }
+        public double Subtraction(double a, double b)
+        {
+            return a +b;
+
+        }
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+
+        }
+        public int Multiplication(int a, int b, int c)
+        {
+            return a * b * c;
+
+        }
+        public double Multiplication(double a, double b)
+        {
+            return a * b;
+
+        }
+    }
+}
---
+++ Calculation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Airthmatical a1=new Airthmatical();
+            int res1 = a1.Addition(2, 3);
+            int res2 = a1.Addition(3, 4,5);
+            double res3 = a1.Addition(4.5, 3.5);
+            Console.WriteLine(res1);
+            Console.WriteLine(res2);
+            Console.WriteLine(res3);
+            int res4 = a1.Subtraction(2, 3);
+            int res5 = a1.Subtraction(3, 4, 5);
+            double res6 = a1.Subtraction(4.5, 6.5);
+            Console.WriteLine(res4);
+            Console.WriteLine(res5);
+            Console.WriteLine(res6);
+            int res7 = a1.Multiplication(4, 5);
+            int res8 = a1.Multiplication(3, 4, 5);
+            double res9 = a1.Multiplication(4.5, 6.5);
+            Console.WriteLine(res7);
+            Console.WriteLine(res8);
+            Console.WriteLine(res9);
+        }
+    }
+}
